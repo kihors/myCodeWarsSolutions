04ee8dacead9535fc1a5d965544e7e448b3395fb
kihors <kihors@gmail.com>
2019-07-12 22:49:17 +0300

    solution 'Convert a String to a Number'

---
+++ src/com/codewars/stringtonumber/TestApp.java
@@ -0,0 +1,9 @@
+package com.codewars.stringtonumber;
+
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println(StringToNumber.stringToNumber("1234"));
+        System.out.println(StringToNumber.stringToNumber("25"));
+        System.out.println(StringToNumber.stringToNumber("-7"));
+    }
+}
